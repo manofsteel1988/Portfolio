3341a8b712d89fa30ea3c18f682a720eb2eb941d
Cyrille Bomba <c.bombangong@fulll.fr>
2022-04-08 16:48:58 +0200

    Ajust about page description

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -32,18 +32,18 @@ const About = () => {
                         />
                     </h1>
                     <p>
-                        I'm very ambitious front-end developer looking for a role in
-                        established IT company with the opportunity to work with the latest
+                        I'm a very ambitious front-end developer looking for a role in
+                        established IT companies with the opportunity to work with the latest
                         technologies on challenging and diverse projects.
                     </p>
                     <p align="LEFT">
                         I'm quietly confident, naturally curious, and perpetually working on
-                        improving my chops one design problem at a time.
+                        improving my skills one design problem at a time.
                     </p>
                     <p>
                         If I need to define myself in one sentence that would be a family
-                        person, father of a beautiful daughter, a sports fanatic,
-                        photography enthusiast, and tech-obsessed!!!
+                        person, father of a three beautiful children, a sports fanatic,
+                        a preacher, and tech-obsessed!!!
                     </p>
                 </div>
 
