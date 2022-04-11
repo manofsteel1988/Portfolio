a5d8300e88634c2b88fd6c1c7925938d84f85a71
Cyrille Bomba <cbngong@gmail.com>
2022-04-11 12:12:24 +0200

    Add upload resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,75 +1,81 @@
 import { useEffect, useState } from 'react'
 import {
-    faCss3,
-    faGitAlt,
-    faHtml5,
-    faJsSquare,
-    faReact,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
 } from '@fortawesome/free-brands-svg-icons'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Resume from '../../assets/resume/Resume_FE_DEV.pdf'
 import './index.scss'
 
 const About = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    useEffect(() => {
-        return setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 3000)
-    }, [])
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
-    return (
-        <>
-            <div className="container about-page">
-                <div className="text-zone">
-                    <h1>
-                        <AnimatedLetters
-                            letterClass={letterClass}
-                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-                            idx={15}
-                        />
-                    </h1>
-                    <p>
-                        I'm a very ambitious front-end developer looking for a role in
-                        established IT companies with the opportunity to work with the latest
-                        technologies on challenging and diverse projects.
-                    </p>
-                    <p align="LEFT">
-                        I'm quietly confident, naturally curious, and perpetually working on
-                        improving my skills one design problem at a time.
-                    </p>
-                    <p>
-                        If I need to define myself in one sentence that would be a family
-                        person, father of a three beautiful children, a sports fanatic,
-                        a preacher, and tech-obsessed!!!
-                    </p>
-                </div>
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I'm a very ambitious front-end developer looking for a role in
+            established IT companies with the opportunity to work with the
+            latest technologies on challenging and diverse projects.
+          </p>
+          <p align="LEFT">
+            I'm quietly confident, naturally curious, and perpetually working on
+            improving my skills one design problem at a time.
+          </p>
+          <p>
+            If I need to define myself in one sentence that would be a family
+            person, father of a three beautiful children, a sports fanatic, a
+            preacher, and tech-obsessed!!!
+          </p>
+          <button className="flat-button">
+            <a href={Resume} download>
+              UPLOAD RESUME
+            </a>
+          </button>
+        </div>
 
-                <div className="stage-cube-cont">
-                    <div className="cubespinner">
-                        <div className="face2">
-                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                        </div>
-                        <div className="face3">
-                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                        </div>
-                        <div className="face4">
-                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-                        </div>
-                        <div className="face5">
-                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-                        </div>
-                        <div className="face6">
-                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
-                        </div>
-                    </div>
-                </div>
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
             </div>
-            <Loader type="pacman" />
-        </>
-    )
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default About
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,116 +7,110 @@ import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
 const Contact = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const form = useRef()
-    const cityPosition = [43.529742, 5.447427]
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const form = useRef()
+  const cityPosition = [43.529742, 5.447427]
 
-    useEffect(() => {
-        return setTimeout(() => {
-            setLetterClass('text-animate-hover')
-        }, 3000)
-    }, [])
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
-    const sendEmail = (e) => {
-        e.preventDefault()
+  const sendEmail = (e) => {
+    e.preventDefault()
 
-        emailjs
-            .sendForm(
-                'gmail',
-                'template_YeJhZkgb',
-                form.current,
-                'your-token'
-            )
-            .then(
-                () => {
-                    alert('Message successfully sent!')
-                    window.location.reload(false)
-                },
-                () => {
-                    alert('Failed to send the message, please try again')
-                }
-            )
-    }
+    emailjs
+      .sendForm('gmail', 'template_YeJhZkgb', form.current, 'your-token')
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
 
-    return (
-        <>
-            <div className="container contact-page">
-                <div className="text-zone">
-                    <h1>
-                        <AnimatedLetters
-                            letterClass={letterClass}
-                            strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
-                            idx={15}
-                        />
-                    </h1>
-                    <p>
-                        I am interested in web development opportunities - especially ambitious or
-                        large projects. However, if you have other request or question,
-                        don't hesitate to contact me using below form either.
-                    </p>
-                    <div className="contact-form">
-                        <form ref={form} onSubmit={sendEmail}>
-                            <ul>
-                                <li className="half">
-                                    <input placeholder="Name" type="text" name="name" required />
-                                </li>
-                                <li className="half">
-                                    <input
-                                        placeholder="Email"
-                                        type="email"
-                                        name="email"
-                                        required
-                                    />
-                                </li>
-                                <li>
-                                    <input
-                                        placeholder="Subject"
-                                        type="text"
-                                        name="subject"
-                                        required
-                                    />
-                                </li>
-                                <li>
-                                    <textarea
-                                        placeholder="Message"
-                                        name="message"
-                                        required
-                                    ></textarea>
-                                </li>
-                                <li>
-                                    <input type="submit" className="flat-button" value="SEND" />
-                                </li>
-                            </ul>
-                        </form>
-                    </div>
-                </div>
-                <div className="info-map">
-                    Cyrille Bomba
-                    <br />
-                    Aix-en-Provence,13100
-                    <br />
-                    France
-                    <br />
-                    <span>cbngong@gmail.com</span>
-                </div>
-                <div className="map-wrap">
-                    <MapContainer center={cityPosition} zoom={6}>
-                        <TileLayer
-                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                        <Marker position={cityPosition}>
-                            <Popup>
-                                This is the city <br />
-                                I live in
-                            </Popup>
-                        </Marker>
-                    </MapContainer>
-                </div>
-            </div>
-            <Loader type="pacman" />
-        </>
-    )
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in web development opportunities - especially
+            ambitious or large projects. However, if you have other request or
+            question, don't hesitate to contact me using below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+        <div className="info-map">
+          Cyrille Bomba
+          <br />
+          Aix-en-Provence,13100
+          <br />
+          France
+          <br />
+          <span>cbngong@gmail.com</span>
+        </div>
+        <div className="map-wrap">
+          <MapContainer center={cityPosition} zoom={6}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={cityPosition}>
+              <Popup>
+                This is the city <br />I live in
+              </Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Contact
